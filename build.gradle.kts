4a1c99729b0b79da98f1fe8b4907f56158445506
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-02-23 00:22:23 +0900

    Setup spotless

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,27 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+import com.diffplug.gradle.spotless.SpotlessPlugin
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.spotless) apply false
+}
+
+subprojects {
+    apply<SpotlessPlugin>()
+
+    extensions.configure<SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            val ktlintVersion = libs.versions.ktlint.get()
+            targetExclude("**/build/**/*.kt")
+            ktlint(ktlintVersion)
+                .editorConfigOverride(
+                    mapOf(
+                        "ktlint_code_style" to "android",
+                        "ij_kotlin_allow_trailing_comma" to true,
+                        "ij_kotlin_allow_trailing_comma_on_call_site" to true,
+                    ),
+                )
+        }
+    }
 }

4c392cb8fe8ea750295c55c52fab5fc378e218ed
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-02-22 23:52:05 +0900

    Add core module

---
+++ core/build.gradle.kts
@@ -0,0 +1,140 @@
+plugins {
+    kotlin("multiplatform")
+}
+
+kotlin {
+    jvm()
+    js(IR) {
+        moduleName = "architectureDICore"
+        nodejs()
+        browser()
+    }
+    ios()
+    macosX64()
+    macosArm64()
+    watchos()
+    tvos()
+
+    mingwX64()
+    mingwX86()
+    linuxX64()
+    linuxArm32Hfp()
+    linuxMips32()
+
+    sourceSets {
+        val commonMain by getting
+
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        val jvmMain by getting {
+            dependsOn(commonMain)
+        }
+
+        val jvmTest by getting {
+            dependsOn(commonTest)
+        }
+
+        val jsMain by getting {
+            dependsOn(commonMain)
+        }
+
+        val jsTest by getting {
+            dependsOn(commonTest)
+        }
+
+        val nativeMain by creating {
+            dependsOn(commonMain)
+        }
+
+        val nativeTest by creating {
+            dependsOn(commonTest)
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        val iosMain by getting {
+            dependsOn(nativeMain)
+        }
+
+        val iosTest by getting {
+            dependsOn(nativeTest)
+        }
+
+        val macosX64Main by getting {
+            dependsOn(nativeMain)
+        }
+
+        val macosX64Test by getting {
+            dependsOn(nativeTest)
+        }
+
+        val macosArm64Main by getting {
+            dependsOn(nativeMain)
+        }
+
+        val macosArm64Test by getting {
+            dependsOn(nativeTest)
+        }
+
+        val watchosMain by getting {
+            dependsOn(nativeMain)
+        }
+
+        val watchosTest by getting {
+            dependsOn(nativeTest)
+        }
+
+        val tvosMain by getting {
+            dependsOn(nativeMain)
+        }
+
+        val tvosTest by getting {
+            dependsOn(nativeTest)
+        }
+
+        val mingwX64Main by getting {
+            dependsOn(nativeMain)
+        }
+
+        val mingwX64Test by getting {
+            dependsOn(nativeTest)
+        }
+
+        val mingwX86Main by getting {
+            dependsOn(nativeMain)
+        }
+
+        val mingwX86Test by getting {
+            dependsOn(nativeTest)
+        }
+
+        val linuxX64Main by getting {
+            dependsOn(nativeMain)
+        }
+
+        val linuxX64Test by getting {
+            dependsOn(nativeTest)
+        }
+
+        val linuxArm32HfpMain by getting {
+            dependsOn(nativeMain)
+        }
+
+        val linuxArm32HfpTest by getting {
+            dependsOn(nativeTest)
+        }
+
+        val linuxMips32Main by getting {
+            dependsOn(nativeMain)
+        }
+
+        val linuxMips32Test by getting {
+            dependsOn(nativeTest)
+        }
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,4 @@ dependencyResolutionManagement {
 }
 
 rootProject.name = "koject"
+include(":core")

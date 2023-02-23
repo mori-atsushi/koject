3258771d36aa4cc373a0aca3ceef6a1f4c6ae9e3
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-02-23 22:55:15 +0900

    Add inject tests

---
+++ integration-test/app/src/commonTest/kotlin/com/moriatsushi/koject/integrationtest/app/InjectTest.kt
@@ -0,0 +1,29 @@
+package com.moriatsushi.koject.integrationtest.app
+
+import com.moriatsushi.koject.Koject
+import com.moriatsushi.koject.inject
+import com.moriatsushi.koject.start
+import kotlin.test.AfterTest
+import kotlin.test.Test
+import kotlin.test.assertIs
+
+class InjectTest {
+    @AfterTest
+    fun clear() {
+        Koject.stop()
+    }
+
+    @Test
+    fun successInject() {
+        Koject.start()
+
+        val appClass1 = inject<AppClass1>()
+        assertIs<AppClass1>(appClass1)
+
+        val appClass2 = inject<AppClass2>()
+        assertIs<AppClass2>(appClass2)
+
+        val appClass3 = inject<AppClass3>()
+        assertIs<AppClass3>(appClass3)
+    }
+}

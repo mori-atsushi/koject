85cc5252ef9184e76f718a29fd0d862b95d0b952
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-02-24 05:32:54 +0900

    Fix tests for Kotlin/JS

--- integration-test/lib/build.gradle.kts
+++ integration-test/lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 kotlin {
     jvm()
     js(IR) {
-        moduleName = "koject-integration-test-app"
+        moduleName = "koject-integration-test-lib"
         nodejs()
         browser()
     }

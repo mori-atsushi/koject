f772e7ed2666da9a5ce51bb9946279efd2c95088
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-02-23 16:35:27 +0900

    Add tests

--- processor/common/src/test/kotlin/com/moriatsushi/koject/processor/compiletesting/KotlinCompilationFactory.kt
+++ processor/common/src/test/kotlin/com/moriatsushi/koject/processor/compiletesting/KotlinCompilationFactory.kt
@@ -2,20 +2,14 @@ package com.moriatsushi.koject.processor.compiletesting
 
 import com.moriatsushi.koject.processor.TestProcessorProvider
 import com.tschuchort.compiletesting.KotlinCompilation
-import com.tschuchort.compiletesting.SourceFile
 import com.tschuchort.compiletesting.kspIncremental
 import com.tschuchort.compiletesting.symbolProcessorProviders
 import java.io.File
 
-class KotlinCompilationFactory(
-    private val workingDir: File,
-) {
-    fun create(
-        vararg sourceFiles: SourceFile,
-    ): KotlinCompilation {
+class KotlinCompilationFactory {
+    fun create(workingDir: File): KotlinCompilation {
         return KotlinCompilation().also {
             it.workingDir = workingDir
-            it.sources = sourceFiles.asList()
             it.inheritClassPath = true
             it.symbolProcessorProviders = listOf(TestProcessorProvider())
             it.kspIncremental = true

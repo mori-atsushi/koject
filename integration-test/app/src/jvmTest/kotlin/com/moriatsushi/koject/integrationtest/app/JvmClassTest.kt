78d1609b0df5dfdb759c16684a06d9c4eb451849
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-02-24 14:04:42 +0900

    Format

--- integration-test/app/src/jvmTest/kotlin/com/moriatsushi/koject/integrationtest/app/JvmClassTest.kt
+++ integration-test/app/src/jvmTest/kotlin/com/moriatsushi/koject/integrationtest/app/JvmClassTest.kt
@@ -3,9 +3,9 @@ package com.moriatsushi.koject.integrationtest.app
 import com.moriatsushi.koject.Koject
 import com.moriatsushi.koject.inject
 import com.moriatsushi.koject.start
+import kotlin.test.assertIs
 import org.junit.After
 import org.junit.Test
-import kotlin.test.assertIs
 
 class JvmClassTest {
     @After

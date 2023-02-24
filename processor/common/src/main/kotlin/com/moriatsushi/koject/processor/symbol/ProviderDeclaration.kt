caf97d69359a67e28f37f36c7e5653396280c581
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-02-25 02:34:48 +0900

    Allow provide from object function

--- processor/common/src/main/kotlin/com/moriatsushi/koject/processor/symbol/ProviderDeclaration.kt
+++ processor/common/src/main/kotlin/com/moriatsushi/koject/processor/symbol/ProviderDeclaration.kt
@@ -1,69 +1,111 @@
 package com.moriatsushi.koject.processor.symbol
 
+import com.google.devtools.ksp.symbol.ClassKind
+import com.google.devtools.ksp.symbol.FunctionKind
 import com.google.devtools.ksp.symbol.KSAnnotated
 import com.google.devtools.ksp.symbol.KSClassDeclaration
+import com.google.devtools.ksp.symbol.KSDeclaration
 import com.google.devtools.ksp.symbol.KSFile
 import com.google.devtools.ksp.symbol.KSFunctionDeclaration
 import com.moriatsushi.koject.internal.identifier.Identifier
 import com.moriatsushi.koject.processor.identifier.of
+import com.squareup.kotlinpoet.ClassName
 import com.squareup.kotlinpoet.MemberName
+import com.squareup.kotlinpoet.MemberName.Companion.member
 import com.squareup.kotlinpoet.TypeName
 import com.squareup.kotlinpoet.ksp.toClassName
 
-internal sealed interface ProviderDeclaration {
-    val identifier: Identifier
+internal sealed class ProviderDeclaration(
+    private val declaration: KSDeclaration,
+    private val function: KSFunctionDeclaration,
+) {
+    abstract val identifier: Identifier
+    abstract val typeName: TypeName
+
     val dependencies: List<DependencyType>
+        get() = function.parameters
+            .map { DependencyType.of(it.type) }
+
     val containingFile: KSFile
-    val typeName: TypeName
+        get() = declaration.containingFile!!
 
     companion object {
         fun of(node: KSAnnotated): ProviderDeclaration? {
             return when (node) {
-                is KSClassDeclaration -> Class(node)
-                is KSFunctionDeclaration -> Function(node)
+                is KSClassDeclaration -> ofClass(node)
+                is KSFunctionDeclaration -> ofFunction(node)
+                else -> null
+            }
+        }
+
+        private fun ofClass(node: KSClassDeclaration): ProviderDeclaration {
+            return Class(node)
+        }
+
+        private fun ofFunction(node: KSFunctionDeclaration): ProviderDeclaration? {
+            return when (node.functionKind) {
+                FunctionKind.TOP_LEVEL -> TopLevelFunction(node)
+                FunctionKind.MEMBER -> {
+                    val parent = node.parentDeclaration as KSClassDeclaration
+                    if (parent.classKind == ClassKind.OBJECT) {
+                        ObjectFunction(parent, node)
+                    } else {
+                        null
+                    }
+                }
                 else -> null
             }
         }
     }
 
-    class Class(private val ksClass: KSClassDeclaration) : ProviderDeclaration {
+    class Class(
+        private val ksClass: KSClassDeclaration,
+    ) : ProviderDeclaration(
+        ksClass,
+        ksClass.primaryConstructor!!,
+    ) {
         override val identifier by lazy {
             Identifier.of(ksClass)
         }
-
-        override val dependencies: List<DependencyType>
-            get() = ksClass.primaryConstructor?.parameters
-                .orEmpty()
-                .map { DependencyType.of(it.type) }
-
-        override val containingFile: KSFile
-            get() = ksClass.containingFile!!
 
         override val typeName: TypeName
             get() = ksClass.toClassName()
     }
 
-    class Function(
-        private val ksFunction: KSFunctionDeclaration,
-    ) : ProviderDeclaration {
-        private val ksType = ksFunction.returnType!!.resolve()
+    class TopLevelFunction(
+        private val function: KSFunctionDeclaration,
+    ) : ProviderDeclaration(function, function) {
+        private val ksType = function.returnType!!.resolve()
 
         override val identifier: Identifier by lazy {
             Identifier.of(ksType)
         }
 
-        override val dependencies: List<DependencyType>
-            get() = ksFunction.parameters
-                .map { DependencyType.of(it.type) }
-
-        override val containingFile: KSFile
-            get() = ksFunction.containingFile!!
-
         val memberName: MemberName
             get() = MemberName(
-                ksFunction.packageName.asString(),
-                ksFunction.simpleName.asString(),
+                function.packageName.asString(),
+                function.simpleName.asString(),
             )
+
+        override val typeName: TypeName
+            get() = ksType.toClassName()
+    }
+
+    class ObjectFunction(
+        private val parent: KSClassDeclaration,
+        private val function: KSFunctionDeclaration,
+    ) : ProviderDeclaration(function, function) {
+        private val ksType = function.returnType!!.resolve()
+
+        override val identifier: Identifier by lazy {
+            Identifier.of(ksType)
+        }
+
+        val parentName: ClassName
+            get() = parent.toClassName()
+
+        val memberName: MemberName
+            get() = parentName.member(function.simpleName.asString())
 
         override val typeName: TypeName
             get() = ksType.toClassName()

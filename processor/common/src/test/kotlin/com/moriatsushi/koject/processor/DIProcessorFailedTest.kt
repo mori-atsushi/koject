abbbe9cd9f0cfa4e479d5d9fe1877faa0888234f
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-02-26 22:57:37 +0900

    Add DIProcessor tests

--- processor/common/src/test/kotlin/com/moriatsushi/koject/processor/DIProcessorFailedTest.kt
+++ processor/common/src/test/kotlin/com/moriatsushi/koject/processor/DIProcessorFailedTest.kt
@@ -2,6 +2,7 @@ package com.moriatsushi.koject.processor
 
 import com.moriatsushi.koject.processor.assert.assertCompileFailed
 import com.moriatsushi.koject.processor.compiletesting.KotlinCompilationFactory
+import com.moriatsushi.koject.processor.error.CodeGenerationException
 import com.moriatsushi.koject.processor.error.NotProvidedException
 import com.moriatsushi.koject.processor.error.WrongScopeException
 import com.tschuchort.compiletesting.SourceFile
@@ -45,9 +46,28 @@ class DIProcessorFailedTest {
         val expectedError = WrongScopeException::class
         val expectedErrorMessage1 =
             "com.testpackage.SingletonScope cannot be created " +
-                "because com.testpackage.NormalScope is not a singleton."
+                    "because com.testpackage.NormalScope is not a singleton."
         val expectedErrorMessage2 =
             "Only a singleton can be requested from a singleton."
+        assertContains(result.messages, expectedError.qualifiedName!!)
+        assertContains(result.messages, expectedErrorMessage1)
+        assertContains(result.messages, expectedErrorMessage2)
+    }
+
+    @Test
+    fun notSupportedAnnotationMemberType() {
+        val folder = tempFolder.newFolder()
+        val complication = compilationFactory.create(folder)
+        complication.sources = listOf(notSupportedAnnotationMemberTypeCode)
+        val result = complication.compile()
+
+        assertCompileFailed(result)
+
+        val expectedError = CodeGenerationException::class
+        val expectedErrorMessage1 =
+            "java.util.ArrayList is an unsupported annotation member type."
+        val expectedErrorMessage2 =
+            "at com.testpackage.ArrayQualifier.array"
         assertContains(result.messages, expectedError.qualifiedName!!)
         assertContains(result.messages, expectedErrorMessage1)
         assertContains(result.messages, expectedErrorMessage2)
@@ -85,6 +105,24 @@ class DIProcessorFailedTest {
                 class SingletonScope(
                     val normal: NormalScope
                 )
+            """,
+    )
+
+    private val notSupportedAnnotationMemberTypeCode = SourceFile.kotlin(
+        "Test.kt",
+        """
+                package com.testpackage
+
+                import com.moriatsushi.koject.Provides
+                import com.moriatsushi.koject.Qualifier
+
+                @Qualifier
+                @Retention(AnnotationRetention.BINARY)
+                annotation class ArrayQualifier(val array: Array<String>)
+
+                @ArrayQualifier(["a", "b", "c"])
+                @Provides
+                fun provideString(): String = "not supported"
             """,
     )
 }

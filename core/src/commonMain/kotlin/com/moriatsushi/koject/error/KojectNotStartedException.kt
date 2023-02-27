887a43ce38491f2da08d089dae21166b44f9322d
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-02-27 10:32:05 +0900

    Update comments

--- core/src/commonMain/kotlin/com/moriatsushi/koject/error/KojectNotStartedException.kt
+++ core/src/commonMain/kotlin/com/moriatsushi/koject/error/KojectNotStartedException.kt
@@ -1,6 +1,8 @@
 package com.moriatsushi.koject.error
 
+import com.moriatsushi.koject.Koject
+
 /**
- * Koject has not been started
+ * [Koject] has not been started
  */
 class KojectNotStartedException(message: String) : Exception(message)

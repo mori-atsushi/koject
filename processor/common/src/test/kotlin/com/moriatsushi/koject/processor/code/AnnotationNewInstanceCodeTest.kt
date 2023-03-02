4fbd78c3a859bb7655d099e2731856a822206614
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-03-02 14:22:03 +0900

    Fix KSAnnotation.toNewInstanceCode()

--- processor/common/src/test/kotlin/com/moriatsushi/koject/processor/code/AnnotationNewInstanceCodeTest.kt
+++ processor/common/src/test/kotlin/com/moriatsushi/koject/processor/code/AnnotationNewInstanceCodeTest.kt
@@ -28,9 +28,9 @@ class AnnotationNewInstanceCodeTest {
             """
                 package com.testpackage
 
-                annotation class Sample
+                annotation class SimpleAnnotation
 
-                @Sample
+                @SimpleAnnotation
                 val value = 0
             """,
         )
@@ -43,9 +43,204 @@ class AnnotationNewInstanceCodeTest {
         assertFileExists(generatedFile)
 
         val expectedCode = """
+            |import com.testpackage.SimpleAnnotation
+            |
+            |public val annotation0: SimpleAnnotation = SimpleAnnotation()
+            |""".trimMargin()
+        assertFileTextEquals(expectedCode, generatedFile)
+    }
+
+    @Test
+    fun longAnnotation() {
+        val folder = tempFolder.newFolder()
+        val complication = compilationFactory.create(folder)
+        val inputCode = SourceFile.kotlin(
+            "Test.kt",
+            """
+                package com.testpackage
+
+                annotation class LongAnnotation(val value: Long)
+
+                @LongAnnotation(100L)
+                val value = 0
+            """,
+        )
+        complication.sources = listOf(inputCode)
+        val result = complication.compile()
+
+        assertCompileSucceed(result)
+
+        val generatedFile = folder.resolve(generated)
+        assertFileExists(generatedFile)
+
+        val expectedCode = """
+            |import com.testpackage.LongAnnotation
+            |
+            |public val annotation0: LongAnnotation = LongAnnotation(value = 100)
+            |""".trimMargin()
+        assertFileTextEquals(expectedCode, generatedFile)
+    }
+
+    @Test
+    fun enumAnnotation() {
+        val folder = tempFolder.newFolder()
+        val complication = compilationFactory.create(folder)
+        val inputCode = SourceFile.kotlin(
+            "Test.kt",
+            """
+                package com.testpackage
+
+                annotation class EnumAnnotation(val value: SampleEnum)
+
+                enum class SampleEnum {
+                    TypeA,
+                    TypeB;
+                }
+
+                @EnumAnnotation(SampleEnum.TypeA)
+                val value = 0
+            """,
+        )
+        complication.sources = listOf(inputCode)
+        val result = complication.compile()
+
+        assertCompileSucceed(result)
+
+        val generatedFile = folder.resolve(generated)
+        assertFileExists(generatedFile)
+
+        val expectedCode = """
+            |import com.testpackage.EnumAnnotation
+            |import com.testpackage.SampleEnum
+            |
+            |public val annotation0: EnumAnnotation = EnumAnnotation(value = SampleEnum.TypeA)
+            |""".trimMargin()
+        assertFileTextEquals(expectedCode, generatedFile)
+    }
+
+    @Test
+    fun classAnnotation() {
+        val folder = tempFolder.newFolder()
+        val complication = compilationFactory.create(folder)
+        val inputCode = SourceFile.kotlin(
+            "Test.kt",
+            """
+                package com.testpackage
+
+                annotation class ClassAnnotation(val value: KClass<*>)
+
+                class Sample
+
+                @ClassAnnotation(Sample::class)
+                val value = 0
+            """,
+        )
+        complication.sources = listOf(inputCode)
+        val result = complication.compile()
+
+        assertCompileSucceed(result)
+
+        val generatedFile = folder.resolve(generated)
+        assertFileExists(generatedFile)
+
+        val expectedCode = """
+            |import com.testpackage.ClassAnnotation
             |import com.testpackage.Sample
             |
-            |public val annotation0: Sample = Sample()
+            |public val annotation0: ClassAnnotation = ClassAnnotation(value = Sample::class)
+            |""".trimMargin()
+        assertFileTextEquals(expectedCode, generatedFile)
+    }
+
+    @Test
+    fun stringAnnotation() {
+        val folder = tempFolder.newFolder()
+        val complication = compilationFactory.create(folder)
+        val inputCode = SourceFile.kotlin(
+            "Test.kt",
+            """
+                package com.testpackage
+
+                annotation class StringAnnotation(val value: String)
+
+                @StringAnnotation("sample-string")
+                val value = 0
+            """,
+        )
+        complication.sources = listOf(inputCode)
+        val result = complication.compile()
+
+        assertCompileSucceed(result)
+
+        val generatedFile = folder.resolve(generated)
+        assertFileExists(generatedFile)
+
+        val expectedCode = """
+            |import com.testpackage.StringAnnotation
+            |
+            |public val annotation0: StringAnnotation = StringAnnotation(value = "sample-string")
+            |""".trimMargin()
+        assertFileTextEquals(expectedCode, generatedFile)
+    }
+
+    @Test
+    fun charAnnotation() {
+        val folder = tempFolder.newFolder()
+        val complication = compilationFactory.create(folder)
+        val inputCode = SourceFile.kotlin(
+            "Test.kt",
+            """
+                package com.testpackage
+
+                annotation class CharAnnotation(val value: Char)
+
+                @CharAnnotation('a')
+                val value = 0
+            """,
+        )
+        complication.sources = listOf(inputCode)
+        val result = complication.compile()
+
+        assertCompileSucceed(result)
+
+        val generatedFile = folder.resolve(generated)
+        assertFileExists(generatedFile)
+
+        val expectedCode = """
+            |import com.testpackage.CharAnnotation
+            |
+            |public val annotation0: CharAnnotation = CharAnnotation(value = 'a')
+            |""".trimMargin()
+        assertFileTextEquals(expectedCode, generatedFile)
+    }
+
+    @Test
+    fun booleanAnnotation() {
+        val folder = tempFolder.newFolder()
+        val complication = compilationFactory.create(folder)
+        val inputCode = SourceFile.kotlin(
+            "Test.kt",
+            """
+                package com.testpackage
+
+                annotation class BooleanAnnotation(val value: Boolean)
+
+                @BooleanAnnotation(false)
+                val value = 0
+            """,
+        )
+        complication.sources = listOf(inputCode)
+        val result = complication.compile()
+
+        assertCompileSucceed(result)
+
+        val generatedFile = folder.resolve(generated)
+        assertFileExists(generatedFile)
+
+        val expectedCode = """
+            |import com.testpackage.BooleanAnnotation
+            |
+            |public val annotation0: BooleanAnnotation = BooleanAnnotation(value = false)
             |""".trimMargin()
         assertFileTextEquals(expectedCode, generatedFile)
     }

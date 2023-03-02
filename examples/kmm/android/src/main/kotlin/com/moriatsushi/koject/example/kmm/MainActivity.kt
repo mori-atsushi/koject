01404802cf3ef22e9490d8408450e73b20207064
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-03-02 16:13:32 +0900

    Format

--- examples/kmm/android/src/main/kotlin/com/moriatsushi/koject/example/kmm/MainActivity.kt
+++ examples/kmm/android/src/main/kotlin/com/moriatsushi/koject/example/kmm/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
--- examples/kmm/android/src/main/kotlin/com/moriatsushi/koject/example/kmm/MyApplicationTheme.kt
+++ examples/kmm/android/src/main/kotlin/com/moriatsushi/koject/example/kmm/MyApplicationTheme.kt
@@ -18,38 +18,38 @@ import androidx.compose.ui.unit.sp
 @Composable
 fun MyApplicationTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
-    content: @Composable () -> Unit
+    content: @Composable () -> Unit,
 ) {
     val colors = if (darkTheme) {
         darkColors(
             primary = Color(0xFFBB86FC),
             primaryVariant = Color(0xFF3700B3),
-            secondary = Color(0xFF03DAC5)
+            secondary = Color(0xFF03DAC5),
         )
     } else {
         lightColors(
             primary = Color(0xFF6200EE),
             primaryVariant = Color(0xFF3700B3),
-            secondary = Color(0xFF03DAC5)
+            secondary = Color(0xFF03DAC5),
         )
     }
     val typography = Typography(
         body1 = TextStyle(
             fontFamily = FontFamily.Default,
             fontWeight = FontWeight.Normal,
-            fontSize = 16.sp
-        )
+            fontSize = 16.sp,
+        ),
     )
     val shapes = Shapes(
         small = RoundedCornerShape(4.dp),
         medium = RoundedCornerShape(4.dp),
-        large = RoundedCornerShape(0.dp)
+        large = RoundedCornerShape(0.dp),
     )
 
     MaterialTheme(
         colors = colors,
         typography = typography,
         shapes = shapes,
-        content = content
+        content = content,
     )
 }

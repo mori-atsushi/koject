182ff333a4fb3feb3e241ad7765f882228560403
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-03-05 16:43:16 +0900

    Using singletons in the todo example app

--- examples/kmm/shared/src/iosMain/kotlin/com/moriatsushi/koject/example/kmm/db/ProvideSqlDriver.ios.kt
+++ examples/kmm/shared/src/iosMain/kotlin/com/moriatsushi/koject/example/kmm/db/ProvideSqlDriver.ios.kt
@@ -3,7 +3,9 @@ package com.moriatsushi.koject.example.kmm.db
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import com.moriatsushi.koject.Provides
+import com.moriatsushi.koject.Singleton
 
+@Singleton
 @Provides
 internal fun provideSqlDriver(): SqlDriver {
     return NativeSqliteDriver(Database.Schema, "todo.db")

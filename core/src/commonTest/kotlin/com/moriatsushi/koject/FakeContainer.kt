dae7f4b174b7d8d31a98a190e44afc250c5f6f86
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-03-06 09:40:56 +0900

    Fix tests

--- core/src/commonTest/kotlin/com/moriatsushi/koject/FakeContainer.kt
+++ core/src/commonTest/kotlin/com/moriatsushi/koject/FakeContainer.kt
@@ -1,10 +1,13 @@
+@file:OptIn(ExperimentalKojectApi::class)
+
 package com.moriatsushi.koject
 
 import com.moriatsushi.koject.internal.Container
+import com.moriatsushi.koject.internal.Extras
 import com.moriatsushi.koject.internal.Identifier
 
 class FakeContainer : Container {
-    override fun resolve(id: Identifier): Any? {
+    override fun resolve(id: Identifier, extras: Extras): Any? {
         return when (id) {
             Identifier.of<Int>() -> 100
             Identifier.of<String>() -> "test"

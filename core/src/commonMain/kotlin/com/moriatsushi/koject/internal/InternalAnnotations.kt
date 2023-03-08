5504ee2bcdc60531b15d0785e66982df6e02ca04
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-03-08 20:50:05 +0900

    Add MustBeDocumented annotations

--- core/src/commonMain/kotlin/com/moriatsushi/koject/internal/InternalAnnotations.kt
+++ core/src/commonMain/kotlin/com/moriatsushi/koject/internal/InternalAnnotations.kt
@@ -42,3 +42,25 @@ annotation class StringComponent(
     AnnotationTarget.PROPERTY,
 )
 annotation class Location(val value: String)
+
+/**
+ * Marks declarations that are internal in Koject API
+ *
+ * Don't use it outside of Koject.
+ * API will change without notice in future releases.
+ */
+@Retention(value = AnnotationRetention.BINARY)
+@Target(
+    AnnotationTarget.CLASS,
+    AnnotationTarget.FUNCTION,
+    AnnotationTarget.TYPEALIAS,
+    AnnotationTarget.PROPERTY,
+)
+@RequiresOptIn(
+    level = RequiresOptIn.Level.ERROR,
+    message = "This is an internal Koject API " +
+            "that should not be used from outside of Koject. " +
+            "API will change without notice in future releases.",
+)
+@MustBeDocumented
+annotation class InternalKojectApi

bd15479876622caec61d7438e72e099e7e6234a1
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-03-10 15:47:18 +0900

    Add android core module

---
+++ android/core/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    alias(libs.plugins.android.library)
+    kotlin("android")
+    alias(libs.plugins.publish)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.dokka)
+}
+
+android {
+    namespace = "com.moriatsushi.koject.android"
+    compileSdk = 33
+    defaultConfig {
+        minSdk = 14
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    buildFeatures {
+        buildConfig = false
+    }
+}
+
+dependencies {
+    api(project(":core"))
+    ksp(project(":processor:lib"))
+}
+
+tasks.dokkaHtmlPartial {
+    moduleName.set("koject-android-core")
+}

7ef39ea8f7cfd287dbc52952e5587b989104a31c
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-03-13 12:37:36 +0900

    Update comments

--- compose/koject-compose-core/src/commonMain/kotlin/com/moriatsushi/koject/compose/ComposeComponent.kt
+++ compose/koject-compose-core/src/commonMain/kotlin/com/moriatsushi/koject/compose/ComposeComponent.kt
@@ -6,6 +6,10 @@ import com.moriatsushi.koject.component.Component
 
 /**
  * Mark it as a type to inject into [Composable].
+ *
+ * Additional available types:
+ * * [kotlinx.coroutines.CoroutineScope] with @[ComposeCoroutineScope]
+ * * android.content.Context with @ComposeContext (android only)
  */
 @OptIn(ExperimentalKojectApi::class)
 @Component

bb5c308906c39bb787550117b962779ceab727e3
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-03-13 16:22:10 +0900

    Add EnumTest

---
+++ integration-test/app/src/commonMain/kotlin/com/moriatsushi/koject/integrationtest/app/Enum.kt
@@ -0,0 +1,20 @@
+package com.moriatsushi.koject.integrationtest.app
+
+import com.moriatsushi.koject.Provides
+
+enum class SampleEnum {
+    Item1,
+    Item2,
+}
+
+@Provides
+fun provideEnum(): SampleEnum {
+    return SampleEnum.Item1
+}
+
+@Provides
+class EnumHolder(
+    val item: SampleEnum,
+)
+
+
---
+++ integration-test/app/src/commonTest/kotlin/com/moriatsushi/koject/integrationtest/app/EnumTest.kt
@@ -0,0 +1,14 @@
+package com.moriatsushi.koject.integrationtest.app
+
+import com.moriatsushi.koject.Koject
+import com.moriatsushi.koject.inject
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class EnumTest {
+    @Test
+    fun successInject() = Koject.runTest {
+        val holder = inject<EnumHolder>()
+        assertEquals(SampleEnum.Item1, holder.item)
+    }
+}

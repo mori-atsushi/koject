ff11e943ff836cb62e9d42268de54fac7d98a60c
Mori-Atsushi <at.sushi.tb@gmail.com>
2023-04-14 22:07:25 +0900

    Update jdk to 17

--- examples/kmm/shared/build.gradle.kts
+++ examples/kmm/shared/build.gradle.kts
@@ -9,7 +9,7 @@ kotlin {
     android {
         compilations.all {
             kotlinOptions {
-                jvmTarget = "11"
+                jvmTarget = "17"
             }
         }
     }
@@ -82,8 +82,8 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility(JavaVersion.VERSION_11)
-        targetCompatibility(JavaVersion.VERSION_11)
+        sourceCompatibility(JavaVersion.VERSION_17)
+        targetCompatibility(JavaVersion.VERSION_17)
     }
 
     buildFeatures {
